f936ff5cdb62e18c9ab4f10a69746c7b49e84aea
Doug Rosvick <drosvick@gmail.com>
2021-10-03 00:20:53 -0600

    v2 - prod API URL

--- project/udagram-frontend/src/environments/environment.ts
+++ project/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a435eeb6a6c6b41ba986248dd8220ae8-897857461.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
